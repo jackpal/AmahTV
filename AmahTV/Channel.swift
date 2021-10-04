3efbdc41e1dba070f3a90f307e9d40a341b01d48
Jack Palevich <jackpal@google.com>
2021-10-03 22:09:58 -0700

    Added currently-playing-channel indicator.

--- AmahTV/Channel.swift
+++ AmahTV/Channel.swift
@@ -7,16 +7,23 @@
 
 import Foundation
 
-struct Channel {
+struct Channel : Equatable, Identifiable {
   var name: String
   var url: URL
+
+  var id: String {
+    name
+  }
 }
 
 extension Channel {
   static let channels = [
-    Channel(name: "民視新聞直播", url: URL(string: "https://www.youtube.com/watch?v=TCnaIE_SAtM")!),
+    Channel(name: "中視新聞", url: URL(string: "https://www.youtube.com/watch?v=TCnaIE_SAtM")!),
     Channel(name: "民視新聞", url: URL(string: "https://www.youtube.com/watch?v=XxJKnDLYZz4")!),
     Channel(name: "三立 LIVE 新聞直播", url: URL(string:"https://www.youtube.com/watch?v=pJ0VgJloR9E")!),
-    Channel(name: "東森新聞", url: URL(string:"https://www.youtube.com/watch?v=IbVnkfVCHRw&list=PLp7hnLHxd1KHiqXmhl4HJWPptOVGzZo2k&t=87s")!)
+    Channel(name: "東森財經", url: URL(string:"https://www.youtube.com/watch?v=zNESDscyCmk")!),
+    Channel(name: "東森新聞", url: URL(string:"https://www.youtube.com/watch?v=IbVnkfVCHRw&list=PLp7hnLHxd1KHiqXmhl4HJWPptOVGzZo2k&t=87s")!),
+    Channel(name: "年代新聞", url: URL(string:"https://www.youtube.com/watch?v=zarN1fpJv4Q&list=PLbyorRThEk_LCt0VzPS8KHsaTI0Y8dboO&t=48s")!)
+
   ]
 }
--- AmahTV/YouTubeView.swift
+++ AmahTV/YouTubeView.swift
@@ -10,14 +10,35 @@ import YouTubePlayerKit
 
 struct YouTubeView: View {
   private let channels = Channel.channels
+  @State
+  private var selectedChannel: Channel?
+
   @StateObject
-  private var youTubePlayer = YouTubePlayer()
+  private var youTubePlayer =
+  YouTubePlayer(
+    configuration: .init(
+      captionLanguage: "zh-Hant",
+      showFullscreenButton: false,
+      language: "zh-Hant",
+      showAnnotations: false,
+      useModestBranding: true
+      )
+  )
 
   var body: some View {
     HStack {
       List(channels, id:\.name) {channel in
-        Button(channel.name) {
+        Button(action: {
           youTubePlayer.source = .url(channel.url.absoluteString)
+          selectedChannel = channel
+        }) {
+          HStack {
+            Text(channel.name)
+            Spacer()
+            if channel == selectedChannel {
+              Image(systemName: "play.fill")
+            }
+          }
         }
       }
       .frame(maxWidth:200)
