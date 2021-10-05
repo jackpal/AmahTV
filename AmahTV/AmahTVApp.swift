06764adea06fbc4079fc4a95953061a11b522f06
Jack Palevich <jackpal@google.com>
2021-10-04 19:04:30 -0700

    Adding in files that had not been added before, removing CoreData

--- AmahTV/AmahTVApp.swift
+++ AmahTV/AmahTVApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct AmahTVApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
           YouTubeView(channels:Channel.channels, initialSelectedChannelIndex: 1)
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
