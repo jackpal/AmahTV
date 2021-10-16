53ec76f99e4a0bd3b6cef773307631ea9bc7c25c
Jack Palevich <jackpal@google.com>
2021-10-16 10:52:01 -0700

    Update channels.
    Throw out any non-live channels.
    Add channels from SquidTV site.

--- AmahTV/Channel.swift
+++ AmahTV/Channel.swift
@@ -9,21 +9,29 @@ import Foundation
 
 struct Channel : Equatable, Hashable, Identifiable {
   var name: String
-  var url: URL
-
-  var id: String {
-    name
+  var url: URL {
+    URL(string:"https://www.youtube.com/watch?v=\(id)")!
   }
+
+  var id: String
 }
+
+/// Channels from https://www.squidtv.net/asia/taiwan/
 
 extension Channel {
   static let channels = [
-    Channel(name: "中視新聞", url: URL(string: "https://www.youtube.com/watch?v=TCnaIE_SAtM")!),
-    Channel(name: "民視新聞", url: URL(string: "https://www.youtube.com/watch?v=XxJKnDLYZz4")!),
-    Channel(name: "三立 LIVE 新聞直播", url: URL(string:"https://www.youtube.com/watch?v=pJ0VgJloR9E")!),
-    Channel(name: "東森財經", url: URL(string:"https://www.youtube.com/watch?v=zNESDscyCmk")!),
-    Channel(name: "東森新聞", url: URL(string:"https://www.youtube.com/watch?v=IbVnkfVCHRw&list=PLp7hnLHxd1KHiqXmhl4HJWPptOVGzZo2k&t=87s")!),
-    Channel(name: "年代新聞", url: URL(string:"https://www.youtube.com/watch?v=zarN1fpJv4Q&list=PLbyorRThEk_LCt0VzPS8KHsaTI0Y8dboO&t=48s")!)
-
+    Channel(name: "中視新聞", id: "TCnaIE_SAtM"),
+    Channel(name: "EBC 東森新聞 51", id: "R2iMq5LKXco"),
+    Channel(name: "民視新聞 53", id: "XxJKnDLYZz4"),
+    Channel(name: "三立 LIVE 新聞直播", id: "CKjSm5ZeehE"),
+    Channel(name: "東森財經", id: "zNESDscyCmk"),
+    Channel(name: "中天新聞24小時", id: "lu_BJKxqGnk"),
+    Channel(name: "TVBS新聞 55", id: "V0WxUakDV7M"),
+    Channel(name: "TVBS選新聞 56", id: "hP6_jfCFxyQ"),
+    Channel(name: "EBC 東森財經新聞 57", id: "ABn_ccXn_jc"),
+    Channel(name: "TTV台視新聞", id: "xL0ch83RAK8"),
+    Channel(name: "CTS華視直播頻道", id: "yUTy1U1wTwE"),
+    Channel(name: "寰宇新聞台", id: "bn-zNDGLgDw"),
+    Channel(name: "信大電視台線上直播", id: "fYcDmJ4XlZ0")
   ]
 }
