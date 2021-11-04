3c1133f8ba40274823e7d5a5820b218127eb37b8
Jack Palevich <jack.palevich@gmail.com>
2021-11-03 21:12:34 -0700

    Channel List.

---
+++ AmahTV/Views/ChannelList.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct ChannelList: View {
+  @ObservedObject public var tv: TV
+  @State var showAddChannelSheet = false
+
+  var body: some View {
+    List {
+      ForEach($tv.channels) { $channel in
+        NavigationLink(destination:
+                        LazyView(EditChannel(channel:$channel))) {
+          Text(channel.name)
+        }
+      }.onDelete {
+        tv.channels.remove(atOffsets: $0)
+        tv.save()
+      }.onMove {
+        tv.channels.move(fromOffsets: $0, toOffset: $1)
+        tv.save()
+      }
+      .onDisappear {
+        tv.save()
+      }
+    }
+    .navigationBarTitle("Channels", displayMode: .inline)
+    .navigationBarItems(trailing:
+                          HStack {
+      EditButton()
+      addChannel
+    })
+  }
+
+  var addChannel: some View {
+    Button(action: {
+      showAddChannelSheet = true
+    }){ Image(systemName: "plus.rectangle").imageScale(.large)
+    }
+    .sheet(isPresented: $showAddChannelSheet) {
+      AddChannelSheet(tv:tv)
+    }
+  }
+}
