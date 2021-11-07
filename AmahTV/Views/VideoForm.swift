8fef672254bd3df18054aa61239952bde3948d12
Jack Palevich <jack.palevich@gmail.com>
2021-11-06 20:21:18 -0700

    An edit that didn't make the previous cl.

--- AmahTV/Views/VideoForm.swift
+++ AmahTV/Views/VideoForm.swift
@@ -1,9 +1,35 @@
 import SwiftUI
 
-enum VideoForm {
-  static let nameLabel = LocalizedStringKey("name-label")
-  static let namePrompt = LocalizedStringKey("name-prompt")
-  static let videoIDLabel = LocalizedStringKey("videoID-label")
-  static let videoIDPrompt = LocalizedStringKey("videoID-prompt")
-  static let previewLabel = LocalizedStringKey("preview-label")
+struct VideoForm : View {
+  @Binding public var video: Video
+  @State private var urlOrVideoID: String = ""
+  @StateObject private var videoMetadata = VideoMetadata()
+
+  init(video: Binding<Video>) {
+    _video = video
+    _urlOrVideoID = State(initialValue:_video.wrappedValue.id)
+  }
+
+  var body: some View {
+    Form {
+      Section(header: Text("name-label")) {
+        TextField("name-prompt", text: $video.name)
+      }
+      Section(header: Text("videoID-label")) {
+        TextField("videoID-prompt", text: $urlOrVideoID)
+      }
+      .onChange(of: urlOrVideoID) { newValue in
+        videoMetadata.resolve(urlOrVideoID: newValue)
+      }
+      .onReceive(videoMetadata.objectWillChange) {_ in
+        video.id = videoMetadata.videoID ?? ""
+      }
+      .onAppear {
+        videoMetadata.resolve(urlOrVideoID: urlOrVideoID)
+      }
+      Section(header: Text("preview-label")) {
+        VideoMetadataView(videoMetadata: videoMetadata)
+      }
+    }
+  }
 }
